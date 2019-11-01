4ddb3e25ddcb88492276926bb1e16ea4c149b20f
oskarvid <oskarvidarsson@gmail.com>
2019-11-01 10:14:38 +0100

    Added updated code

--- scripts/RunTsdSbatchPipeline.sh
+++ scripts/RunTsdSbatchPipeline.sh
@@ -6,28 +6,24 @@ declare -a STEP2
 # Print the time the pipeline started
 date
 
-# Path to the directory for the sumstats_info.v2.txt and sample2inputs.csv files
-INPUT_PATH=/cluster/projects/p172/oskar/GRSWorkflow/input-definitions
+source settings/settings.conf
 
 # Enter the number of phenotypes for step 1 and samples for step 2
-PHENO_JOBS=2
-SAMPLE_JOBS=2
-
-# The bash scripts need to know what the project directory is
-PROJ_DIR=/cluster/projects/p172/oskar/GRSWorkflow
+PHENO_JOBS=$(( $(wc -l $INPUT_PATH/sumstats_info.v2.txt | awk '{ print $1 }') - 1 ))
+SAMPLE_JOBS=$(wc -l $INPUT_PATH/step2inputs.csv | awk '{ print $1 }')
 
 # For loop that queues all jobs for step 1 and puts the job id for each job in an array to handle dependencies for step 3 execution
 for i in $(seq 1 ${PHENO_JOBS}); do
-	STEP1[$i]+="$(sbatch scripts/step1.sbatch $INPUT_PATH $i $PROJ_DIR | awk {'print $4'})"
+	STEP1[$i]+="$(sbatch ${S1SETTINGS[@]} scripts/step1.sbatch $i | awk {'print $4'})"
 done
 
 # For loop that queues all jobs for step 2 and puts the job id for each job in an array to handle dependencies for step 3 execution
 for j in $(seq 1 ${SAMPLE_JOBS}); do
-	STEP2[$j]+="$(sbatch scripts/step2.sbatch $INPUT_PATH $j $PROJ_DIR | awk {'print $4'})"
+	STEP2[$j]+="$(sbatch ${S2SETTINGS[@]} scripts/step2.sbatch $j | awk {'print $4'})"
 done
 
 for i in $(seq 1 ${PHENO_JOBS}); do
 	for j in $(seq 1 ${SAMPLE_JOBS}); do
-			sbatch --dependency=afterok:${STEP1[$i]}:${STEP2[$j]} scripts/step3.sbatch $INPUT_PATH $j $i $PROJ_DIR
+		sbatch ${S3SETTINGS[@]} --dependency=afterok:${STEP1[$i]}:${STEP2[$j]} scripts/step3.sbatch $j $i
 	done
 done
--- scripts/rm-outputs.sh
+++ scripts/rm-outputs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+rm -rf results/*/*
 rm -rf data/sumstats/postqc/*
 rm -rf data/geno/postqc/s*/*
 rm -rf intermediate/*
